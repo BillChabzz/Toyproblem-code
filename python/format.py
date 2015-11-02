49e01c91f6d101e04d4a16a2ca95c407684b16e1
BillChabzz <chaby9.8@gmail.com>
2015-11-02 09:23:58 +0300

    format python solution

---
+++ python/format.py
@@ -0,0 +1,6 @@
+#FORMAT STRING
+#Using PYTHON make a program that can format any number to add a comma to its appropriate position
+#For example (1000000) becomes (1,000,000)
+def format(x):
+  	return "{:3}".format(x)
+    return "%d%s" %(x,result)    
